91fcd9e0b7183c5f9256b79454807ff890616099
Joshua Garcia <joshua_garcia@yahoo.com>
2013-10-18 12:55:18 -0700

    increased sleep by 5 secs

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ CLASSPATH=.:HelloWorld.jar:lib/junit-4.11.jar:lib/hamcrest-core-1.3.jar
 java -cp $CLASSPATH org.junit.runner.JUnitCore com.test.HelloWorldTest
 
 #sleep to emulate time spent
-sleep 30
+sleep 35
 
