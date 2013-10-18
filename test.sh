888f7c45dc1264be3a068525945987577989cba9
Joshua Garcia <joshua_garcia@yahoo.com>
2013-10-17 21:30:47 -0700

    adding dummy sleeep

--- test.sh
+++ test.sh
@@ -3,3 +3,7 @@
 CLASSPATH=.:HelloWorld.jar:lib/junit-4.11.jar:lib/hamcrest-core-1.3.jar
 
 java -cp $CLASSPATH org.junit.runner.JUnitCore com.test.HelloWorldTest
+
+#sleep to emulate time spent
+sleep 30
+
